1beb329f3f43d978d56c3666744e2063cf9ed6f1
Ishan <agarwal.ishanedu@gmail.com>
2026-04-19 21:43:23 -0400

    p2 - made aliens drop

--- alien.py
+++ alien.py
@@ -29,9 +29,11 @@ class Alien(Sprite):
         temp_speed = self.settings.fleet_speed
         if self.check_edges():
             self.settings.fleet_direction *= -1
+            self.x -= self.settings.fleet_drop_speed 
         
         self.y += temp_speed * self.settings.fleet_direction
         self.rect.y = self.y
+        self.rect.x = self.x
 
     def check_edges(self) -> bool:
         return(self.rect.bottom >= self.boundaries.bottom or self.rect.top <= 0)
